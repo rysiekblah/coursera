bd908af8501da60bf0bf136748be6a9de994367d
Tomek Kozlowski <tkozlowski007@gmail.com>
2015-05-10 17:27:06 +0800

    FRP wek3 server

--- reactive-programming/nodescala/src/main/scala/nodescala/nodescala.scala
+++ reactive-programming/nodescala/src/main/scala/nodescala/nodescala.scala
@@ -29,7 +29,12 @@ trait NodeScala {
    *  @param token        the cancellation token
    *  @param body         the response to write back
    */
-  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = ???
+  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = {
+    while (response.hasNext && token.nonCancelled) {
+      exchange.write(response.next())
+    }
+    if(token.nonCancelled) exchange.close()
+  }
 
   /** A server:
    *  1) creates and starts an http listener
@@ -41,7 +46,14 @@ trait NodeScala {
    *  @param handler        a function mapping a request to a response
    *  @return               a subscription that can stop the server and all its asynchronous operations *entirely*
    */
-  def start(relativePath: String)(handler: Request => Response): Subscription = ???
+  def start(relativePath: String)(handler: Request => Response): Subscription = {
+    val listener = createListener(relativePath)
+    val listenerSubscription = listener.start()
+
+
+
+    listenerSubscription
+  }
 
 }
 
